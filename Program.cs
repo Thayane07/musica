ca69a15f15851550fdfc14050de92d0fb423b2ee
thayane <meu-email@meu-servidor.com>
2026-03-27 22:18:48 -0300

    musica

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("A Barata diz que tem");
+Thread.Sleep(1000);
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Sete saias de filó");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("É mentira da barata");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Ela tem é uma só"); 
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Ha, ha, ha, ho, ho, ho"); 
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Ela tem é uma só"); 
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Há, há, há, hó, hó, hó"); 
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Ela tem é uma só"); 
+Thread.Sleep(1000);
+
+Console.ResetColor();
